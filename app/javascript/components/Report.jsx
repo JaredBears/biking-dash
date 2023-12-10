fb5313c78e2aab0617bb0a81d7f428ad13c2c55e
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-12-10 16:46:23 -0600

    Redo Reports with React pt 3

---
+++ app/javascript/components/Report.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+const Report = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [report, setReport] = useState({});
+
+  useEffect(() => {
+    const url = `/api/v1/reports/${params.id}`;
+    fetch(url)
+      .then((data) => {
+        if (data.ok) {
+          return data.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((data) => setReport(data))
+      .catch(() => navigate("/reports"));
+    }, [params.id]);
+
+    return (
+      <div className="">
+        <div className="hero position-relative d-flex align-items-center justify-content-center">
+          <img src={report.images[0]} alt={`${report.category} image`} className="img-fluid position-absolute" />
+          <div className="overlay bg-dark position-absolute" />
+          <h1 className="display-4 position-relative text-white">
+            {report.category}
+          </h1>
+        </div>
+        <div className="container py-5">
+          <ul>
+            <li>Category: {report.category}</li>
+            <li>Reported on: {report.created_at}</li>
+            <li>Location: {report.location}</li>
+            <li>Description: {report.description}</li>
+          </ul>
+        </div>
+      </div>
+    )
+};
+
+export default Report;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import Reports from "../components/Reports";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/reports" element={<Reports />} />
     </Routes>
   </Router>
 );
