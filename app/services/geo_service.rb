7818068b1bac0c23c1af9045cbb39e3eccaef3f1
Jared Bears <jaredbears@outlook.com>
2023-12-22 15:51:03 -0600

    Moved Basic Logic into Services I've copied the basic logic into the services, but I still need to refactor to use the services and to make sure everything still works.

--- app/services/geo_service.rb
+++ app/services/geo_service.rb
@@ -1,4 +1,33 @@
 class GeoService
+
+  #This service is to interact with the geocode api.  It will take an
+  #address and return the coordinates, or take coordinates and return the
+  #address.  This is a free service so long as I only use it less than once
+  #per second.
+
   def initialize
+    @base_url = "https://geocode.maps.co/"
+  end
+
+  def geo_encode(address)
+    uri = URI("#{@base_url}search?q=#{address}")
+    pp uri
+    response = Net::HTTP.get(uri)
+    pp response
+    json = JSON.parse(response)
+    pp json
+    coord = {
+      lat: json[0]["lat"].to_f,
+      lon: json[0]["lon"].to_f,
+    }
+    return coord
+  end
+
+  def reverse_geo(lat, lon)
+    uri = URI("#{@base_url}reverse?lat=#{lat}&lon=#{lon}")
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+    address = json["address"]
+    return address
   end
 end
--- app/services/whu_service.rb
+++ app/services/whu_service.rb
@@ -1,4 +1,41 @@
 class WhuService
+
   def initialize
+    @url = "https://whitehouseuprising.github.io/maps2/data/chicago/all.json"
+    @starting = 0
+    @ending = 0
+    @new_reports = {}
   end
+
+  def import_whu_data()
+    uri = URI("https://whitehouseuprising.github.io/maps2/data/chicago/all.json")
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+
+    json.sort_by! { |report| report.dig("properties", "id") }
+    @ending = json.last.dig("properties", "id")
+
+    first_index = json.find_index { |report| report.dig("properties", "id") == @starting }
+    json = json[first_index..-1]
+    json.each do |report|
+      pp "adding report #{[report.dig("properties", "id")]} to new_reports..."
+      if report.dig("properties", "obstruction")[0..4] == "Other"
+        report.dig("properties", "obstruction")[0..-1] = "Other  (damaged lane / snow / debris / pedestrian / etc.)"
+      end
+      @new_reports["reports"][report.dig("properties", "id")] = {
+        blu_id: report.dig("properties", "id"),
+        category: report.dig("properties", "obstruction"),
+        lat: report.dig("geometry", "coordinates")[1],
+        lon: report.dig("geometry", "coordinates")[0],
+      }
+      pp "#{@new_reports["reports"][report.dig("properties", "id")]} added"
+    end
+    if Rails.env.development?
+      pp "saving new_reports to json file..."
+      File.open("new_reports#{Date.Today}.json", "w") do |f|
+        f.write(@new_reports.to_json)
+      end
+    end
+  end
+  
 end
